fce351d87da687a2530baf627ef50eb5107e27dc
Robin Delbaere <hello@rdelbaere.fr>
2024-03-29 16:15:37 +0100

    Add webpush on backend

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,23 @@
 import express from "express";
 import cors from "cors";
+import webpush from "web-push";
+import dotenv from "dotenv";
 import { Server } from "socket.io";
 
-const messages = [];
+// Load .env file
+dotenv.config();
 
+// Configure webpush with keys
+webpush.setVapidDetails(
+    'mailto:hello@rdelbaere.fr',
+    process.env.VAPID_PUBLIC_KEY,
+    process.env.VAPID_PRIVATE_KEY
+);
+
+const messages = [];
+const subscriptions = [];
+
+// API
 const app = express();
 app.use(cors());
 app.use(express.json());
@@ -12,6 +26,20 @@ app.get('/messages', (req, res) => {
     return res.json(messages);
 });
 
+app.get('/push/key', (req, res) => {
+    return res.json({
+        pubkey: process.env.VAPID_PUBLIC_KEY
+    });
+});
+
+app.post('/push/sub', (req, res) => {
+    subscriptions.push(req.body);
+    return res.ok;
+});
+
+app.listen(4000);
+
+// Websocket
 const server = new Server({
     cors: {
         origin: "*",
@@ -25,5 +53,4 @@ server.on("connection", socket => {
     });
 });
 
-app.listen(4000);
 server.listen(3000);
